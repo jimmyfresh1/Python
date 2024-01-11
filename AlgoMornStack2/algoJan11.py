4805443ce5cfa36b36c200108e7c736eb3106bcf
jimmyfresh1 <jamesfern14@gmail.com>
2024-01-11 11:18:54 -0600

    python algoJan11

---
+++ AlgoMornStack2/algoJan11.py
@@ -0,0 +1,18 @@
+def reverse_check(string):
+    rev_string= string[::-1]
+    if rev_string==string:
+        return True
+    else:
+        return False
+
+txt = "anything"
+txt2 = "lolol"
+print(reverse_check(txt))
+print(reverse_check(txt2))
+
+# print(reverse_check("doada"))
+# if txt==reverse_check(txt2):
+    # print("True")
+# def reverse_check("lol")
+#     # txtfor= "Hello World"
+#     # txt = txtfor[::-1]
