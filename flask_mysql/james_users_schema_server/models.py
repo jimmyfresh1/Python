77bf5c6962c92d90ea52731bad0e4356af359eac
jimmyfresh1 <jamesfern14@gmail.com>
2024-01-21 03:37:44 -0600

    fixed up final stuff

--- flask_mysql/james_users_schema_server/models.py
+++ flask_mysql/james_users_schema_server/models.py
@@ -7,6 +7,17 @@ class Model:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+
+
+    @classmethod
+    def create(cls, form_data):
+        query = """
+            INSERT INTO users (first_name, last_name, email) 
+            VALUES(%(first_name)s, %(last_name)s, %(email)s);
+        """
+        return connectToMySQL("users_schema").query_db(query, form_data)
+    
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -16,10 +27,26 @@ class Model:
         for user in results:
             users.append(cls(user) )
         return users
+    
     @classmethod
-    def create(cls, form_data):
-        query = """
-            INSERT INTO users (first_name, last_name, email) 
-            VALUES(%(first_name)s, %(last_name)s, %(email)s);
-        """
+    def get_one(cls,id):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL('users_schema').query_db(query, {"id":id})
+        if results:
+            row=results[0]
+            one_user=cls(row)
+            return one_user
+
+
+    @classmethod
+    def update (cls, form_data):
+        query = """UPDATE users
+            SET first_name=%(first_name)s, last_name =%(last_name)s, email = %(email)s 
+            WHERE id = %(id)s"""
         return connectToMySQL("users_schema").query_db(query, form_data)
+
+    @classmethod
+    def delete(cls, id):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        data = {"id": id}
+        return connectToMySQL("users_schema").query_db(query, {"id":id})
--- flask_mysql/james_users_schema_server/server.py
+++ flask_mysql/james_users_schema_server/server.py
@@ -12,14 +12,32 @@ def new_character():
 
 @app.route("/process_character_create", methods=["POST"] )
 def create_user():
-    Model.create(request.form)
-    return redirect('/read_all')
+    id= Model.create(request.form)
+    return redirect(f'/show/{id}')
 
 @app.route("/read_all")    
 def read_all():
     users = Model.get_all()
     return render_template("read_all.html", users=users)
 
+@app.route("/show/<int:id>")    
+def show_user(id):
+    user=Model.get_one(id)
+    return render_template("show.html", user=user)
+
+@app.route("/edit/<int:id>")
+def edit_user(id):
+    user = Model.get_one(id)
+    return render_template("edit.html", user=user)
+@app.route("/process_character_edit", methods=["POST"] )
+def edit_user_process():
+    Model.update(request.form)
+    user_id=request.form['id']
+    return redirect(f'/show/{user_id}')
+@app.route("/delete/<int:id>")
+def delete(id):
+    Model.delete(id)
+    return redirect('/read_all')
 if __name__ == "__main__":
     app.run(debug=True)
 
