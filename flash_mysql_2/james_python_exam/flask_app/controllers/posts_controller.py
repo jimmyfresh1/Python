2df8686d1cd490c9a673c63f29bdc852c9c698cd
jimmyfresh1 <jamesfern14@gmail.com>
2024-01-25 19:37:26 -0600

    remove python exam

--- flash_mysql_2/james_python_exam/flask_app/controllers/posts_controller.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_app import app
-from flask_app.models.post_model import Sasighting
--- flash_mysql_2/james_python_exam/flask_app/models/post_model.py
+++
@@ -1,7 +0,0 @@
-from flask_app.config.mysqlconnection import connectToMySQL
-from flask_app.models import user_model
-DATABASE = "sightings_schema"
-
-class Sasighting:
-    def __init__(self,data):
-        self.id= data['id']
